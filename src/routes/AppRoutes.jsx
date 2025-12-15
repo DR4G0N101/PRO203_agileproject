d774e5af7c7029230d43ec788ec1c92194567484
Ibrahim Khan <shiraishi93@gmail.com>
2025-12-16 00:12:35 +0100

    react router dom link

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,32 +1,32 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
 // settings
-import SettingsLayout from "../pages/settings/SettingsLayout";
-import SettingsMain from "../pages/settings/SettingsMain";
-import Preferences from "../pages/settings/subpages/Preferences";
-import ContactHuskelista from "../pages/settings/subpages/ContactHuskelista";
-import RegisterNewAccess from "../pages/settings/subpages/RegisterNewAccess";
-import ChangePassword from "../pages/settings/subpages/ChangePassword";
-import DeleteAccount from "../pages/settings/subpages/DeleteAccount";
-import Privacy from "../pages/settings/subpages/Privacy";
+import SettingsLayout from "../pages/SettingsLayout";
+import SettingsMain from "../pages/SettingsMain";
+
+// subpages
+import Preferences from "../pages/subpages/Preferences";
+import Contact from "../pages/subpages/Contact";
+import Register from "../pages/subpages/Register";
+import ChangePassword from "../pages/subpages/ChangePassword";
+import DeleteAccount from "../pages/subpages/DeleteAccount";
+import Privacy from "../pages/subpages/Privacy";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      {/* Start appen rett i settings */}
       <Route path="/" element={<Navigate to="/settings" replace />} />
 
       <Route path="/settings" element={<SettingsLayout />}>
         <Route index element={<SettingsMain />} />
         <Route path="preferanser" element={<Preferences />} />
-        <Route path="kontakt-huskelista" element={<ContactHuskelista />} />
-        <Route path="registrer-ny-tilgang" element={<RegisterNewAccess />} />
+        <Route path="kontakt-huskelista" element={<Contact />} />
+        <Route path="registrer-ny-tilgang" element={<Register />} />
         <Route path="endre-passord" element={<ChangePassword />} />
         <Route path="slett-konto" element={<DeleteAccount />} />
         <Route path="personvern" element={<Privacy />} />
       </Route>
 
-      {/* fallback */}
       <Route path="*" element={<div>404 – Fant ikke siden</div>} />
     </Routes>
   );
