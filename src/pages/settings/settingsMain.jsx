09d7d045746dd81c7010830f99bcb1d92c7d0227
Ibrahim Khan <shiraishi93@gmail.com>
2025-12-16 00:29:53 +0100

    coupling settings

--- src/pages/settings/settingsMain.jsx
+++ src/pages/settings/settingsMain.jsx
@@ -1,8 +1,35 @@
+import { useNavigate } from "react-router-dom";
+
 export default function SettingsMain() {
+  const navigate = useNavigate();
+
   return (
     <div>
-      <h1>✅ SettingsMain</h1>
-      <p>Dette er hovedsiden for innstillinger.</p>
+      <h1>Innstillinger</h1>
+
+      <button onClick={() => navigate("preferences")}>
+        Preferanser
+      </button>
+
+      <button onClick={() => navigate("contact")}>
+        Kontakt Huskelista
+      </button>
+
+      <button onClick={() => navigate("register")}>
+        Registrer ny tilgang
+      </button>
+
+      <button onClick={() => navigate("change-password")}>
+        Endre passord
+      </button>
+
+      <button onClick={() => navigate("account")}>
+        Slett konto
+      </button>
+
+      <button onClick={() => navigate("privacy")}>
+        Personvern
+      </button>
     </div>
   );
 }
